e795c38e948aaed6268455cfeddc5f9b414e041b
joeliberal <joe.libral2693@gmail.com>
2022-04-08 00:24:24 +0430

    start

---
+++ shop/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import TemplateView
+# Create your views here.
+from django.urls import translate_url
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
